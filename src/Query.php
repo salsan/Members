793400ba9ea25e21846480f571cdb67e3b967efb
Salvatore Santagati <me@salsan.dev>
2024-04-22 10:23:56 +0200

    feat: added support new web interface

--- src/Query.php
+++ src/Query.php
@@ -5,22 +5,24 @@ declare(strict_types=1);
 namespace Salsan\Members;
 
 use Salsan\Utils\String\HiddenSpaceTrait;
-
+use Salsan\Utils\DOM\DOMDocumentTrait;
 use DOMDocument;
+use DOMXPath;
 
 class Query
 {
     use HiddenSpaceTrait;
+    use DOMDocumentTrait;
 
     private DOMDocument $dom;
-    private string $url = "https://www.federscacchi.it/str_tess.php";
+    private string $url = "https://www.federscacchi.com/fsi/index.php/struttura/tesserati";
 
-    function __construct(array $params)
+    public function __construct(array $params)
     {
         $this->dom = new DOMDocument();
-        libxml_use_internal_errors(true);
 
-        $id = $params['id'] ?? '';
+        // Members Parameters
+        $idx = $params['idx'] ?? '';
         $cgn = $params['surname'] ?? '';
         $gen = $params['gender'] ?? '';
         $an1 = $params['bornFrom'] ?? '';
@@ -28,141 +30,143 @@ class Query
         $cat = $params['category'] ?? '';
         $pro = $params['province'] ?? '';
         $reg = $params['region'] ?? '';
+        $tiptes = $params['membershipType'] ?? '';
+        $anno = $params['membershipYear'] ?? '';
+        // Club Parameters
         $socpro = $params['clubProvince'] ?? '';
         $socreg = $params['clubRegion'] ?? '';
         $cir = $params['clubId'] ?? '';
-        $anno = $params['membershipYear'] ?? '';
-        $tiptes = $params['membershipType'] ?? '';
+        // Order Parameters
         $ord = $params['order'] ?? '';
         $senso = $params['sense'] ?? '';
 
-        $curl = curl_init();
+        $this->url .=
+            "?idx={$idx}" .
+            "&cgn={$cgn}" .
+            "&gen={$gen}" .
+            "&an1={$an1}" .
+            "&an2={$an2}" .
+            "&cat={$cat}" .
+            "&pro={$pro}" .
+            "&reg={$reg}" .
+            "&tiptes={$tiptes}" .
+            "&anno={$anno}" .
+            "&socpro={$socpro}" .
+            "&socreg={$socreg}" .
+            "&cir={$cir}" .
+            "&ord={$ord}" .
+            "&senso={$senso}" .
+            "&ric=1" .
+            "&tabella=tabella";
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array(
-                'id' => $id,
-                'cgn' => $cgn,
-                'gen' => $gen,
-                'an1' => $an1,
-                'an2' => $an2,
-                'cat' => $cat,
-                'pro' => $pro,
-                'reg' => $reg,
-                'socpro' => $socpro,
-                'socreg' => $socreg,
-                'cir' => $cir,
-                'anno' => $anno,
-                'ord' => $ord,
-                'senso' => $senso,
-                'ric' => '1'
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-        $this->dom->loadHTML($response);
+        $this->dom = $this->getHTML($this->url, null);
     }
 
     public function getNumber(): array
     {
-        $row = $this->dom
-            ->getElementsByTagName("table")
-            ->item(4)
-            ->getElementsByTagName("td");
+        $xpath = new DOMXPath($this->dom);
 
-
-        preg_match_all(
-            "/\d+/",
-            $row[count($row) - 1]->textContent,
-            $matches,
-            PREG_SET_ORDER,
-            0,
+        $total = $this->getNodeValue(
+            $xpath,
+            '//table[@class="table table-striped table-hover"]//following::div[@class="alert alert-success"]//b[1]'
         );
 
-        $members = ["total" => $matches[0][0], "rookie" => $matches[1][0]];
+        $rookie = $this->getNodeValue(
+            $xpath,
+            '//table[@class="table table-striped table-hover"]//following::div[@class="alert alert-success"]//b[2]'
+        );
+
+        $members = ["total" => $total, "rookie" => $rookie];
 
         return $members;
     }
 
     public function getList(): iterable
     {
-        $row = $this->dom
-            ->getElementsByTagName("table")
-            ->item(4)
-            ->getElementsByTagName("tr");
+        $xpath = new DOMXPath($this->dom);
+        $total_rows = $xpath->evaluate('count(//tr[@bgcolor="#03A83A"]/following-sibling::tr)');
 
         $members = [];
+        $row = 0;
 
-        foreach ($row as $user) {
-            if ($user->getElementsByTagName("td")->length > 14) {
-                //  id
-                $id = $user->getElementsByTagName("td")[0]->textContent;
-                //  Surname and Name
-                $members[$id]["name"] = $this->replaceWithStandardSpace($user->getElementsByTagName(
-                    "td",
-                )[1]->textContent);
-                // Rookie (boolean)
-                $members[$id]["isRookie"] = (bool) $user->getElementsByTagName("td")[2]
-                    ->textContent;
-                //  year subscribe
-                $members[$id]["year_subscribe"] = $user->getElementsByTagName(
-                    "td",
-                )[3]->textContent;
-                //  gender
-                $members[$id]["gender"] = $user->getElementsByTagName(
-                    "td",
-                )[4]->textContent;
-                //  born date
-                $members[$id]["birthday"] = $user->getElementsByTagName(
-                    "td",
-                )[5]->textContent;
-                //  category
-                $members[$id]["category"] = $user->getElementsByTagName(
-                    "td",
-                )[6]->textContent;
-                //  province
-                $members[$id]["province"] = $user->getElementsByTagName(
-                    "td",
-                )[7]->textContent;
-                //  region
-                $members[$id]["region"] = $this->replaceWithStandardSpace($user->getElementsByTagName(
-                    "td",
-                )[8]->textContent);
-                //  citizenship
-                $members[$id]["citizenship"] = $user->getElementsByTagName(
-                    "td",
-                )[9]->textContent;
-                //  club id
-                $members[$id]["club_id"] = $user->getElementsByTagName(
-                    "td",
-                )[10]->textContent;
-                //  club name
-                $members[$id]["club_name"] = $this->replaceWithStandardSpace($user->getElementsByTagName(
-                    "td",
-                )[11]->textContent);
-                //  club city
-                $members[$id]["club_city"] = $this->replaceWithStandardSpace($user->getElementsByTagName(
-                    "td",
-                )[12]->textContent);
-                //  club region
-                $members[$id]["club_region"] = $this->replaceWithStandardSpace($user->getElementsByTagName(
-                    "td",
-                )[13]->textContent);
-                //  members type and number
-                $members[$id]["card_number"] = $this->replaceWithStandardSpace($user->getElementsByTagName(
-                    "td",
-                )[14]->textContent);
-            }
+        for ($i = 0; $total_rows > $i; $i++) {
+            $row = $row + 1;
+            $xpath_member = '//tr[@bgcolor]/following-sibling::tr[' . $row . ']';
+
+            //  id
+            $id = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[1]'
+            );
+            //  Surname and Name
+            $members[$id]["name"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[2]'
+            );
+            // Rookie (boolean)
+            $members[$id]["isRookie"] = (bool) $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[3]'
+            );
+            //  year subscribe
+            $members[$id]["year_subscribe"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[4]'
+            );
+            //  gender
+            $members[$id]["gender"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[5]'
+            );
+            //  born date
+            $members[$id]["birthday"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[6]'
+            );
+            //  category
+            $members[$id]["category"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[7]'
+            );
+            //  province
+            $members[$id]["province"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[8]'
+            );
+            $members[$id]["region"] =  $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[9]'
+            );
+            //  citizenship
+            $members[$id]["citizenship"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[10]'
+            );
+            //  club id
+            $members[$id]["club_id"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[11]'
+            );
+            //  club name
+            $members[$id]["club_name"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[12]'
+            );
+            //  club city
+            $members[$id]["club_city"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[13]'
+            );
+            //  club region
+            $members[$id]["club_region"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[14]'
+            );
+            //  members type and number
+            $members[$id]["card_number"] = $this->getNodeValue(
+                $xpath,
+                $xpath_member . '//td[15]'
+            );
         }
 
         return $members;
